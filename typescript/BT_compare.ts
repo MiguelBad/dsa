ac3eea59076431067fea62ec2f665bcf7a6e2ef4
miguel <badayosbiboy@gmail.com>
2024-08-13 13:21:24 +0930

    binary tree compare

---
+++ typescript/BT_compare.ts
@@ -0,0 +1,31 @@
+import { BTNode, createTree, createTree2 } from './binary_tree'
+
+function compare(leaf1: BTNode<number>, leaf2: BTNode<number>,): boolean {
+    if (leaf1 === null && leaf2 === null) {
+        return true;
+    }
+
+    if (leaf1 === null || leaf2 === null) {
+        return false;
+    }
+
+    if (leaf1.item !== leaf2.item) {
+        return false;
+    }
+
+    return compare(leaf1.left, leaf2.left) && compare(leaf1.right, leaf2.right)
+}
+
+function compareTree(): void {
+    // similar compare
+    const same1: BTNode<number> = createTree();
+    const same2: BTNode<number> = createTree();
+    console.log(compare(same1, same2));
+
+    // similar compare
+    const dif1: BTNode<number> = createTree();
+    const dif2: BTNode<number> = createTree2();
+    console.log(compare(dif1, dif2));
+}
+
+compareTree();
--- typescript/binary_tree.ts
+++ typescript/binary_tree.ts
@@ -53,3 +53,53 @@ export function createTree(): BTNode<number> {
 
     return node1;
 }
+
+export function createTree2(): BTNode<number> {
+    const createNode = (item: number): BTNode<number> => ({
+        item,
+        left: null,
+        right: null
+    })
+
+    const node1 = createNode(1);
+    const node2 = createNode(2);
+    const node3 = createNode(3);
+    const node4 = createNode(4);
+    const node5 = createNode(5);
+    const node6 = createNode(6);
+    const node7 = createNode(7);
+    const node8 = createNode(8);
+    const node9 = createNode(9);
+    const node10 = createNode(20);
+    const node11 = createNode(21);
+    const node12 = createNode(22);
+    const node13 = createNode(23);
+
+    node1.left = node2;
+    node1.right = node3;
+
+    node2.left = node4;
+    node2.right = node5;
+
+    node3.left = node6;
+    node3.right = node7;
+
+    node4.left = node8;
+    node4.right = node9;
+
+    node5.left = node10;
+    node5.right = node11;
+
+    node6.right = node12;
+    node6.right = node13;
+
+    //          __  1  __
+    //         /         \
+    //        2           3
+    //      /   \       /   \
+    //     4     5     6     7 
+    //    / \   / \   / \   / \
+    //   8   9 20 21 22 23 -   -
+
+    return node1;
+}
