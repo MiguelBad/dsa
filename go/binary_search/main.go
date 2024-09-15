0af5f4a2c67e7d42864edcd034f04cda6a08ee17
MiguelBad <badayosbiboy@gmail.com>
2024-09-16 01:44:04 +0930

    binary search

--- go/binary_search/main.go
+++ go/binary_search/main.go
@@ -2,8 +2,35 @@ package main
 
 import "fmt"
 
-func binarySearch() {
+func binarySearch(arr []int, needle int) int {
+	low := 0
+	high := len(arr) - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+
+		if arr[mid] == needle {
+			return mid
+		} else if arr[mid] > needle {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+
+	return -1
 }
 
 func main() {
+	arr := []int{1, 5, 12, 16, 20, 24, 38, 40, 48}
+
+	for _, needle := range []int{5, 1, 33, 48} {
+		idx := binarySearch(arr, needle)
+
+		if idx == -1 {
+			fmt.Printf("Number: %d\nNot found\n\n", needle)
+		} else {
+			fmt.Printf("Number: %d\nAt index: %d\n\n", needle, idx)
+		}
+	}
 }
