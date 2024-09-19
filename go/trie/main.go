9dc4547e58f117f6ec2145ad3203e6d860503509
MiguelBad <badayosbiboy@gmail.com>
2024-09-19 19:57:54 +0930

    semi working trie

--- go/trie/main.go
+++ go/trie/main.go
@@ -19,41 +19,86 @@ type DataType struct {
 }
 
 type TrieNode struct {
-	letter rune
-	isWord bool
-	child  *TrieNode
+	children map[rune]*TrieNode
+	isWord   bool
 }
 
 type TrieTree struct {
-    root [26]string
+	root *TrieNode
 }
 
-var Data []DataType
-
-func readJson() {
+func readJson() []DataType {
 	file, err := os.ReadFile("data.json")
 	if err != nil {
 		panic(fmt.Sprintf("Error opening json file\n%v\n", err))
 	}
 
-	err = json.Unmarshal(file, &Data)
+	var data []DataType
+	err = json.Unmarshal(file, &data)
 	if err != nil {
 		panic(fmt.Sprintf("Error decoding json file\n%v\n", err))
 	}
+
+	return data
 }
 
-func insertData(root *TrieTree) {
-	for _, phrase := range Data {
-		if phrase.English != nil {
-			engList := []rune(*phrase.English)
+func (t *TrieTree) Insert(phrase string) {
+	node := t.root
 
+	test := &TrieNode{children: make(map[rune]*TrieNode), isWord: false}
+	node.children[200] = test
+
+	for _, char := range phrase {
+		if _, ok := node.children[char]; !ok {
+			node.children[char] = &TrieNode{
+				children: make(map[rune]*TrieNode),
+				isWord:   false,
+			}
 		}
+
+		node = node.children[char]
 	}
+
+	node.isWord = true
+}
+
+func (t *TrieTree) Search(phrase string) bool {
+	node := t.root
+
+	for _, char := range phrase {
+		if _, ok := node.children[char]; !ok {
+			return false
+		}
+
+        node = node.children[char]
+        fmt.Println(string(char))
+	}
+
+    return node.isWord
 }
 
 func main() {
-	readJson()
+	trie := &TrieTree{root: &TrieNode{children: make(map[rune]*TrieNode), isWord: false}}
+	data := readJson()
 
-	root := &TrieTree{}
-	insertData(root)
+	for _, phrase := range data {
+		if phrase.English != nil {
+			trie.Insert(*phrase.English)
+		}
+
+		if phrase.Dharug != nil {
+			trie.Insert(*phrase.Dharug)
+		}
+
+		if phrase.GlossEnglish != nil {
+			trie.Insert(*phrase.GlossEnglish)
+		}
+
+		if phrase.GlossDharug != nil {
+			trie.Insert(*phrase.GlossDharug)
+		}
+	}
+
+    fmt.Println(trie.Search("you"))
+
 }
