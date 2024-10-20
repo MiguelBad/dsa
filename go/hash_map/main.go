e0bf0499b9f1fcd5b4896e68da936e9b1d502da2
MiguelBad <badayosbiboy@gmail.com>
2024-10-20 22:48:56 +0930

    update

--- go/hash_map/main.go
+++ go/hash_map/main.go
@@ -1,39 +1,69 @@
 package main
 
 import (
-// "fmt"
-// "hash"
+	// "fmt"
+	"hash/fnv"
+	"strconv"
 )
 
-type ListNode[K any, V any] struct {
+type Key interface {
+	string | int
+}
+
+type Value interface {
+	string | int
+}
+
+type ListNode[K Key, V Value] struct {
 	key  K
 	val  V
 	next *ListNode[K, V]
 }
 
-type HashMap[K any, V any] struct {
-	maxLen  int
-	hashMap []ListNode[any, any]
+type HashMap[K Key, V Value] struct {
+	maxLen  uint64
+	hashMap []ListNode[K, V]
 }
 
-func newHashMap(maxLen int) *HashMap[any, any] {
-	return &HashMap[any, any]{
-		maxLen:  31,
-		hashMap: make([]ListNode[any, any], maxLen),
+func newHashMap[K Key, V Value]() *HashMap[K, V] {
+	const maxLen uint64 = 31
+	return &HashMap[K, V]{
+		maxLen:  maxLen,
+		hashMap: make([]ListNode[K, V], maxLen),
 	}
 }
 
 func (H *HashMap[K, V]) insert(key K, value V) {
+	idx := H._hashIdx(key)
+	item := H.hashMap[idx]
+
+	if item.key == key {
+	}
 }
 
 func (H *HashMap[K, V]) get() {
 }
 
-func (H *HashMap[K, V]) _hashIdx() {
+func (H *HashMap[K, V]) _hashIdx(key K) uint64 {
+	hash := fnv.New64()
+	var keyBytes []byte
+
+	switch v := any(key).(type) {
+	case int:
+		keyBytes = []byte(strconv.Itoa(v))
+	case string:
+		keyBytes = []byte(v)
+	default:
+		panic("unsupported type")
+	}
+
+	hash.Write(keyBytes)
+	return hash.Sum64() % H.maxLen
 }
 
 func (H *HashMap[K, V]) _reHash() {
 }
 
 func main() {
+	// hashMap := newHashMap[string, int]()
 }
