35f54871799088c6e6bdffcb32c6be44293c9c58
MiguelBad <badayosbiboy@gmail.com>
2024-10-27 16:53:14 +0930

    hash map semi done

--- go/hash_map/main.go
+++ go/hash_map/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"hash/fnv"
 	"strconv"
 )
@@ -10,60 +10,111 @@ type Key interface {
 	string | int
 }
 
-type Value interface {
-	string | int
+type HashItem[K Key] struct {
+	key   K
+	value any
+	next  *HashItem[K]
 }
 
-type ListNode[K Key, V Value] struct {
-	key  K
-	val  V
-	next *ListNode[K, V]
+type HashMap[K Key] struct {
+	table     []*HashItem[K]
+	length    int
+	maxLength int
 }
 
-type HashMap[K Key, V Value] struct {
-	maxLen  uint64
-	hashMap []ListNode[K, V]
-}
-
-func newHashMap[K Key, V Value]() *HashMap[K, V] {
-	const maxLen uint64 = 31
-	return &HashMap[K, V]{
-		maxLen:  maxLen,
-		hashMap: make([]ListNode[K, V], maxLen),
+func newHashMap[K Key]() *HashMap[K] {
+	return &HashMap[K]{
+		maxLength: 101,
+		table:     make([]*HashItem[K], 101),
 	}
 }
 
-func (H *HashMap[K, V]) insert(key K, value V) {
-	idx := H._hashIdx(key)
-	item := H.hashMap[idx]
-
-	if item.key == key {
+func newHashItem[K Key](key K, val any) *HashItem[K] {
+	return &HashItem[K]{
+		key:   key,
+		value: val,
 	}
 }
 
-func (H *HashMap[K, V]) get() {
+func (h *HashMap[K]) set(key K, val any) {
+	item := newHashItem(key, val)
+	idx := h.hashIdx(key)
+
+	if h.table[idx] == nil {
+		h.table[idx] = item
+	} else {
+		curr := h.table[idx]
+		for curr.next != nil {
+			curr = curr.next
+		}
+		curr.next = item
+	}
+
+	h.length++
 }
 
-func (H *HashMap[K, V]) _hashIdx(key K) uint64 {
-	hash := fnv.New64()
-	var keyBytes []byte
+func (h *HashMap[K]) get(key K) any {
+	idx := h.hashIdx(key)
 
-	switch v := any(key).(type) {
+	for curr := h.table[idx]; curr != nil; curr = curr.next {
+		if curr.key == key {
+			return curr.value
+		}
+	}
+
+	fmt.Printf("%v not found\n", key)
+	return nil
+}
+
+func (h *HashMap[K]) hashIdx(key K) int {
+	hash := fnv.New64a()
+	var keyByte []byte
+
+	switch k := any(key).(type) {
 	case int:
-		keyBytes = []byte(strconv.Itoa(v))
+		keyByte = []byte(strconv.Itoa(k))
 	case string:
-		keyBytes = []byte(v)
+		keyByte = []byte(k)
 	default:
-		panic("unsupported type")
+		panic("Key must be an int or a string")
 	}
 
-	hash.Write(keyBytes)
-	return hash.Sum64() % H.maxLen
+	for i := 0; i < 3; i++ {
+		_, err := hash.Write(keyByte)
+
+		if err != nil {
+			fmt.Println("Failed to create hash")
+			continue
+		}
+
+		hashVal := hash.Sum64()
+		return int(hashVal % uint64(h.maxLength))
+	}
+
+	fmt.Println("Failed to create hash value 3 times. Using 0 as the key")
+	return 0
 }
 
-func (H *HashMap[K, V]) _reHash() {
+func (h *HashMap[K]) reHash() {
+}
+
+func (h *HashMap[K]) keys() []K {
+	keys := make([]K, 0, h.length)
+	for _, item := range h.table {
+		for curr := item; curr != nil; curr = curr.next {
+			keys = append(keys, curr.key)
+		}
+	}
+
+	return keys
+}
+
+func (h *HashMap[K]) values(key K, val any) {
+}
+
+func (h *HashMap[K]) entries(key K, val any) {
 }
 
 func main() {
-	// hashMap := newHashMap[string, int]()
+	hashMap := newHashMap[int]()
 }
